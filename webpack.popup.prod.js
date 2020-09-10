a9d7c5a4eccc5c945c03ede6b9a987daaadacd75
Number9 <david.janitzek@gmail.com>
2020-09-10 20:16:07 +0200

    Added fontawesome localy

--- webpack.popup.prod.js
+++ webpack.popup.prod.js
@@ -5,7 +5,7 @@ const ENTRY_DASHBOARD = {
     out: path.resolve(__dirname, './Distribution/Popup/')
 };
 
-const version = 'v3.1.0';
+const version = 'v3.1.1';
 
 const config = {
     mode: 'production',
